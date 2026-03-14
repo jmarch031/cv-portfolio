10ff55e87bfbceb0531f55479a483c54dcdd7b45
Julien Marchand <julien.marchand@decathlon.com>
2026-03-14 12:25:23 +0100

    fix: use standard paths for vite build

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   // Base path for GitHub Pages deployment
   // Replace 'cv-portfolio' with your repository name if it's different
-  base: './',
+  base: '/cv-portfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
